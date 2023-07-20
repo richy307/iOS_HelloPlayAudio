1e4bff3cd802c72440f732f0bc6ceea175d841b1
Richy <richy@webcomm.com.tw>
2023-07-20 11:25:38 +0800

    import AVFoundation, Bundle.main.path -> URL -> AVAudioPlayer

--- iOS_HelloPlayAudio/ViewController.swift
+++ iOS_HelloPlayAudio/ViewController.swift
@@ -4,8 +4,11 @@
 //
 //  Created by 王麒翔 on 2023/7/20.
 //
+//  先 import Right.mp3 檔案
+
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,6 +18,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // find sound path
+        if let path = Bundle.main.path(forResource: "Right", ofType: "mp3") {
+            
+            // let url = URL(fileURLWithPath: path) // 背拋棄
+            let url2 = URL(filePath: path)
+            
+            do{
+                let audioPlayer = try AVAudioPlayer(contentsOf: url2)
+            } catch {
+                
+            }
+        }
+        
     }
 
 
