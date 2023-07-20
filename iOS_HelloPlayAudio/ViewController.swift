db47e93ddd287e28c7af38a1de0d18ab32c7a762
Richy <richy@webcomm.com.tw>
2023-07-20 11:24:18 +0800

    create button, connect button

--- iOS_HelloPlayAudio/ViewController.swift
+++ iOS_HelloPlayAudio/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func play(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
