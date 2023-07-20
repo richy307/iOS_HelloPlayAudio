036273a5dfed7ccdda4acd31a62bd994f4c79c81
Richy <richy@webcomm.com.tw>
2023-07-20 11:35:57 +0800

    AVAudioPlayer 調整屬性

--- iOS_HelloPlayAudio/ViewController.swift
+++ iOS_HelloPlayAudio/ViewController.swift
@@ -11,22 +11,36 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // 宣告 ViewController 屬性
+    var audioPlayer: AVAudioPlayer?
+    
 
     @IBAction func play(_ sender: UIButton) {
+        audioPlayer?.stop() // 播放停止
+        audioPlayer?.currentTime = 0 // 音樂目前時間
+        audioPlayer?.play() // 播放音樂
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // find sound path
+        // 讀取音檔
+        // find sound path // 檔案位置路徑
         if let path = Bundle.main.path(forResource: "Right", ofType: "mp3") {
             
+            // URL 資料格式
             // let url = URL(fileURLWithPath: path) // 背拋棄
             let url2 = URL(filePath: path)
             
             do{
-                let audioPlayer = try AVAudioPlayer(contentsOf: url2)
+                audioPlayer = try AVAudioPlayer(contentsOf: url2)
+                // 調整屬性
+                audioPlayer?.enableRate = true // 可調速率
+                audioPlayer?.rate = 0.5 // 速率
+                audioPlayer?.numberOfLoops = -1 // -1, 0, 1, 2, ... // 播放次數
+                audioPlayer?.volume = 0.3 // 音量
             } catch {
                 
             }
